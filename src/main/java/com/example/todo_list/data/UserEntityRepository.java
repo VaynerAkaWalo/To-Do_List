ac342312a4844ff1edfc3f9c20993e6c94ea9626
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-04-11 13:05:52 +0200

    UserEntity and repository

---
+++ src/main/java/com/example/todo_list/data/UserEntityRepository.java
@@ -0,0 +1,12 @@
+package com.example.todo_list.data;
+
+import com.example.todo_list.models.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserEntityRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+
+    boolean existsUserEntityByUsername(String username);
+}
