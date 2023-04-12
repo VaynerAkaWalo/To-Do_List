f2dfba7fd789de7f491e8b79243c1c69b8f506f5
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-04-12 18:53:59 +0200

    Templates & css/js

---
+++ src/main/resources/static/js/homeScript.js
@@ -0,0 +1,37 @@
+let changeStatusButtons = document.getElementsByClassName("changeStatus")
+let tasks = document.getElementsByClassName("task")
+let showCompleted = document.getElementById("showCompleted")
+
+for (const changeStatusButton of changeStatusButtons) {
+    changeStatusButton.addEventListener("click", () => {
+        fetch("/task?id=" + changeStatusButton.getAttribute('taskid'), {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        }).then(window.location.reload.bind(window.location))
+    })
+}
+
+function filterTasks() {
+    if (showCompleted.checked === true) {
+        for (const task of tasks) {
+            task.style.display = "flex"
+        }
+    }
+    else {
+        for (const task of tasks) {
+            if (task.getAttribute("status") === "Completed") {
+                task.style.display = "none"
+            }
+            else {
+                task.style.display = "flex"
+            }
+        }
+    }
+}
+
+filterTasks()
+showCompleted.addEventListener("change", () => {
+    filterTasks();
+})
