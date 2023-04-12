c7eecf78bbdd05679a5f8dc7a7b0cb1904a4b664
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-04-12 18:51:58 +0200

    HomeController & removed home from View controller

---
+++ src/main/java/com/example/todo_list/web/HomeController.java
@@ -0,0 +1,31 @@
+package com.example.todo_list.web;
+
+import com.example.todo_list.data.TasksService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    private final TasksService tasksService;
+
+    public HomeController(TasksService tasksService) {
+        this.tasksService = tasksService;
+    }
+
+    @ModelAttribute
+    public void getTasks(Model model, Principal principal) {
+        model.addAttribute("tasks", tasksService.getUserTasks(principal.getName()));
+    }
+
+    @GetMapping
+    public String getHomePage() {
+        return "home";
+    }
+}
