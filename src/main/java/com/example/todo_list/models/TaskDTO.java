e33d5f94e9cdae83cd00de6eb077c120787406b9
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-07-31 23:09:55 +0200

    Changed DTOtotask function now function need UserEntity as parameter

--- src/main/java/com/example/todo_list/models/TaskDTO.java
+++ src/main/java/com/example/todo_list/models/TaskDTO.java
@@ -2,9 +2,13 @@ package com.example.todo_list.models;
 
 public record TaskDTO(String name, String details) {
 
-    public Task TaskDTOtoTask(Task task) {
+    public Task TaskDTOtoTask(UserEntity userEntity) {
+        Task task = new Task();
+
         task.setName(this.name);
         task.setDetails(this.details);
+        task.setUserEntity(userEntity);
+
         return task;
     }
 }
