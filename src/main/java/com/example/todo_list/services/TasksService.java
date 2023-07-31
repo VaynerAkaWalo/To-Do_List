aa1b67422d6f5afbac56fced086ce0877301b907
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-07-31 23:11:43 +0200

    Refactorization: now TaskService is responsible for manipulating taskRepository

--- src/main/java/com/example/todo_list/services/TasksService.java
+++ src/main/java/com/example/todo_list/services/TasksService.java
@@ -1,10 +1,18 @@
 package com.example.todo_list.services;
 
 import com.example.todo_list.models.Task;
+import com.example.todo_list.models.TaskDTO;
 import com.example.todo_list.models.UserEntity;
 
 import java.util.List;
 
 public interface TasksService {
-    public List<Task> getUserTasks(String username);
+
+    void addTask(TaskDTO taskDTO, String username);
+
+    void deleteTask(Long id, String username);
+
+    void editTask(Long id, TaskDTO taskDTO, String username);
+
+    List<Task> getAllTasksByUser(String username);
 }
--- src/main/java/com/example/todo_list/services/TasksServiceImp.java
+++ src/main/java/com/example/todo_list/services/TasksServiceImp.java
@@ -1,5 +1,9 @@
 package com.example.todo_list.services;
 
+import com.example.todo_list.exceptions.TaskNotFoundException;
+import com.example.todo_list.exceptions.UnauthorizedException;
+import com.example.todo_list.exceptions.UserNotFoundException;
+import com.example.todo_list.models.TaskDTO;
 import com.example.todo_list.respositories.TaskRepository;
 import com.example.todo_list.respositories.UserEntityRepository;
 import com.example.todo_list.models.Task;
@@ -8,27 +12,60 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TasksServiceImp implements TasksService {
 
-    private TaskRepository taskRepository;
-    private UserEntityRepository userEntityRepository;
+    private final TaskRepository taskRepository;
+    private final UserEntityRepository userEntityRepository;
 
     public TasksServiceImp(TaskRepository taskRepository, UserEntityRepository userEntityRepository) {
         this.taskRepository = taskRepository;
         this.userEntityRepository = userEntityRepository;
     }
 
-    public List<Task> getUserTasks(String username) {
-        if (username == null) {
-            return new ArrayList<>();
+    @Override
+    public void addTask(TaskDTO taskDTO, String username) {
+        Optional<UserEntity> userEntityOptional= userEntityRepository.findByUsername(username);
+        UserEntity user = userEntityOptional.orElseThrow(UserNotFoundException::new);
+
+        taskRepository.save(taskDTO.TaskDTOtoTask(user));
+    }
+
+    @Override
+    public void deleteTask(Long id, String username) {
+        Optional<Task> taskOptional = taskRepository.findById(id);
+        Task task = taskOptional.orElseThrow(TaskNotFoundException::new);
+
+        if (!task.getUserEntity().getUsername().equals(username)) {
+            throw new UnauthorizedException();
         }
 
-        UserEntity user = userEntityRepository.findByUsername(username);
+        taskRepository.deleteById(id);
+    }
+
+    @Override
+    public void editTask(Long id, TaskDTO taskDTO, String username) {
+        Optional<Task> taskOptional = taskRepository.findById(id);
+        Task task = taskOptional.orElseThrow(TaskNotFoundException::new);
+
+        if (!task.getUserEntity().getUsername().equals(username)) {
+            throw new UnauthorizedException();
+        }
+
+        Optional<UserEntity> userEntityOptional= userEntityRepository.findByUsername(username);
+        UserEntity user = userEntityOptional.orElseThrow(UserNotFoundException::new);
+
+        taskRepository.save(taskDTO.TaskDTOtoTask(user));
+    }
+
+    @Override
+    public List<Task> getAllTasksByUser(String username) {
+        Optional<UserEntity> userEntityOptional= userEntityRepository.findByUsername(username);
+        UserEntity user = userEntityOptional.orElseThrow(UserNotFoundException::new);
 
         return taskRepository.getAllByUserEntity(user);
     }
-
 
 }
